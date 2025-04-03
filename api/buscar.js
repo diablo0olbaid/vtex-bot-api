4d4f9c7be7c5655488b3b23bab800371889e453d
diablo0olbaid <104808307+diablo0olbaid@users.noreply.github.com>
2025-04-03 19:25:46 -0300

    buscar.js
    toma sellers

--- api/buscar.js
+++ api/buscar.js
@@ -11,6 +11,7 @@ export default async function handler(req, res) {
   const cuenta = 'carrefourar'
   const dominio = 'vtexcommercestable.com.br'
   const pedido = req.query.query || 'remera'
+  const sellerIdFiltro = req.query.sellerId || '1'
 
   try {
     const url = `https://${cuenta}.${dominio}/api/catalog_system/pub/products/search?ft=${encodeURIComponent(pedido)}`
@@ -22,21 +23,29 @@ export default async function handler(req, res) {
 
     const data = await response.json()
 
-    const primeros = data.slice(0, 3).map(p => {
-      const sku = p.items?.[0]?.itemId || null
-      const offer = p.items?.[0]?.sellers?.[0]?.commertialOffer || {}
+    const productosFiltrados = data
+      .map(p => {
+        const item = p.items?.[0]
+        const seller = item?.sellers?.find(s => s.sellerId === sellerIdFiltro)
 
-      return {
-        nombre: p.productName,
-        link: `https://${cuenta}.${dominio}/${p.linkText}/p`,
-        imagen: p.items?.[0]?.images?.[0]?.imageUrl || '',
-        sku,
-        precio: offer.Price || null,
-        precioAnterior: offer.ListPrice || null
-      }
-    })
+        if (!seller || seller?.commertialOffer?.AvailableQuantity < 1) {
+          return null
+        }
 
-    res.status(200).json({ productos: primeros })
+        return {
+          nombre: p.productName,
+          link: `https://${cuenta}.${dominio}/${p.linkText}/p`,
+          imagen: item.images?.[0]?.imageUrl || '',
+          sku: item.itemId,
+          precio: seller.commertialOffer?.ListPrice || null,
+          precioOferta: seller.commertialOffer?.Price || null
+        }
+      })
+      .filter(Boolean)
+      .slice(0, 3)
+
+    res.status(200).json({ productos: productosFiltrados })
+
   } catch (error) {
     res.status(500).json({ error: error.message })
   }
